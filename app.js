4dec227a05b595541fed60e119a54fd28c2fbe18
ImpuhweStella <stellahimpuhwe346@gmail.com>
2024-05-20 15:38:28 +0200

    adding swagger documentation

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const blogRoutes = require('./routes/blogRoutes');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 dotenv.config();
 connectDB();
@@ -14,5 +16,8 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
-const PORT = process.env.PORT || 5000;
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
